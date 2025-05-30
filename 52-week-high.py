cbfa5ceca548dde5e349ff2c3d40ce7331d7bab6
mishakura <79343312+mishakura@users.noreply.github.com>
2025-05-30 15:59:53 -0300

    yoo

--- 52-week-high.py
+++
@@ -1,6 +0,0 @@
-"""52-week-high momentum: The 52-week-high portfolio takes long (short)
-positions in stocks whose current price is close to (far from) the 52-week
-high. The distance from the 52-week high is measured by the price of
-the stock one month ago divided by the 52-week high in the previous
-year. So if we are standing on December 31, 2015, we divide the price on
-November 30, 2015, by the 52-week high November 30, 2014–2015"""
