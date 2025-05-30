b92a0c8429c24524463d73d47706ca15ca346ec3
mishakura <79343312+mishakura@users.noreply.github.com>
2025-05-30 15:45:01 -0300

    new algos dudeee

---
+++ industry_momentum.py
@@ -0,0 +1,4 @@
+"""Industry momentum: In 1999, Toby Moskowitz and Mark Grinblatt12
+develop an industry momentum screen. The universe of stocks is split
+into 20 industries, and a value-weight return is computed for each industry. The industry momentum portfolio takes long (short) position in
+stocks in the 30 percent top (bottom) performing industries."""
